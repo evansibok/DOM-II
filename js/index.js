67c956158d3d3818ea0052d4f8b4b74a3593572e
evansibok <evansibok@gmail.com>
2019-09-03 15:18:56 +0100

    added resize event

--- js/index.js
+++ js/index.js
@@ -6,18 +6,19 @@
 //  []`resize`
 //  []`scroll`
 //  []`select`
-//  []`dblclick`
 
+let mainNav = document.querySelector(".main-navigation");
+let navLinks = document.querySelectorAll("a");
 let navLink1 = document.querySelector(".nav-link:nth-of-type(1)");
 let navLink2 = document.querySelector(".nav-link:nth-of-type(2)");
-// let h1 = document.querySelector("h1.logo-heading");
 let headImage = document.querySelector(".intro img");
+let h1 = document.querySelector(".logo-heading");
+
 
 // Using `mouseover` 1
 navLink1.addEventListener('mouseover', (e) => {
     e.target.style.color = "teal";
 });
-
 
 // Using `keydown` 2
 document.addEventListener('keydown', (event) => {
@@ -39,6 +40,19 @@ window.onoffline = function(){
 headImage.ondblclick = function(e){
     e.target.style.display = "none";
 }
+
+// Using `resize` 6
+function navBlack() {
+    mainNav.style["background-color"] = "black";
+    navLinks.forEach((ele) => {
+        ele.style.color = "white";
+    })
+    h1.style.color = "white";
+}
+window.addEventListener('resize', navBlack);
+
+// 7
+
 
 
 // Stopping Navigation from propagating
