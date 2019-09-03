4669d051428661d7cde7adc0b0ed2e71bb7645d8
evansibok <evansibok@gmail.com>
2019-09-03 13:36:58 +0100

    created mouseover event

--- js/index.js
+++ js/index.js
@@ -1,1 +1,32 @@
 // Your code goes here
+
+//  []`mouseover`
+//  []`keydown`
+//  []`wheel`
+//  []`drag / drop`
+//  []`load`
+//  []`focus`
+//  []`resize`
+//  []`scroll`
+//  []`select`
+//  []`dblclick`
+
+let navLink2 = document.querySelector(".nav-link:nth-of-type(1)");
+
+// Using `mouseover`
+navLink2.addEventListener('mouseover', (e) => {
+    e.target.style.color = "teal";
+});
+
+
+
+
+
+
+
+
+
+// Using `load`
+// document.addEventListener('load', (e) => {
+//     console.log("Load has been used!");
+// })
